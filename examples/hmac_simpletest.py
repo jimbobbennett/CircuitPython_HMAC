098a1ada749e9d8bcfdd0e483edd3b8f332c814b
Jim Bennett <jim.bennett@microsoft.com>
2020-03-07 12:49:31 -0800

    Update hmac_simpletest.py

--- examples/hmac_simpletest.py
+++ examples/hmac_simpletest.py
@@ -1,7 +1,6 @@
 import adafruit_hmac as hmac
-import adafruit_hashlib as hashlib
 
 secret = "secret"
 msg = "message"
 
-key = hmac.new(secret, msg=msg, digestmod=hashlib.sha256).digest()
+key = hmac.new(secret, msg=msg).digest()
